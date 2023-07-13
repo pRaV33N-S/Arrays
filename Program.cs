0bf9b1017655ea0088a4874eeb87f0cfd57c458a
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-13 12:20:48 +0530

    practice commit

--- Program.cs
+++ Program.cs
@@ -10,28 +10,36 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int [,] marks= new int[4, 3];
+
+            int [,] marks= new int[4, 5];
             for(int i = 0; i < 4; i++)
             {
                 Console.WriteLine($"Enter Student {i+1}\'s Roll number");
                 marks[i, 0] = int.Parse(Console.ReadLine());
-                for(int j = 1; j < 3; j++)
+                for(int j = 1; j < 5; j++)
                 {
                     Console.WriteLine($"Enter marks of the Student\'s{i + 1} in Sem {j}");
                     marks[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             Console.WriteLine("Marks List as follows");
-            Console.WriteLine("RollNo \t SemOne \t SemTwo");
+            Console.WriteLine("RollNo SemOne SemTwo SemThree SemFour Total");
             for(int i = 0; i < 4; i++)
             {
-                for(int j = 0; j < 3; j++)
-                {
+                for(int j = 0; j < 5; j++)
+                { 
                     Console.Write(marks[i, j] + " \t");
                 }
+                int total = 0;
+                for (int k = 1; k < 5; k++)
+                {
+                    total = total + marks[i, k];
+                }
+                Console.Write(total);
                 Console.WriteLine("");
             }
-            Console.ReadKey();
+            
+         
         }
     }
 }
